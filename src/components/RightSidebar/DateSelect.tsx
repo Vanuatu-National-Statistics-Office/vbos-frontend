1445b72b263abae0d58f0e98aea7faf835fd9745
LanesGood <Lane@developmentseed.org>
2025-10-23 12:51:03 -0400

    Change date selection to sliderrange

--- src/components/RightSidebar/DateSelect.tsx
+++ src/components/RightSidebar/DateSelect.tsx
@@ -1,34 +1,54 @@
 import { useDateStore } from "@/store/date-store";
-import { NumberInput, HStack, IconButton } from "@chakra-ui/react";
-import { LuMinus, LuPlus } from "react-icons/lu";
+import { Flex, Slider } from "@chakra-ui/react";
 
 export const DateSelect = () => {
+  const minYear = 2004;
   const maxYear = new Date().getFullYear() - 1;
   const { year, setYear } = useDateStore();
+  const marks = [
+    { value: minYear, label: minYear.toString() },
+    { value: maxYear, label: maxYear.toString() },
+  ];
 
   return (
-    <NumberInput.Root
-      unstyled
-      spinOnPress={true}
-      min={2004}
+    <Slider.Root
+      width="100%"
+      min={minYear}
       max={maxYear}
-      value={year || "2024"}
-      onValueChange={(e) => setYear(e.value)}
+      defaultValue={[typeof year === "number" ? year : 2024]}
+      onValueChange={(e) => setYear(String(e.value[0]))}
+      colorPalette="blue"
+      variant="solid"
     >
-      <NumberInput.Label>Year</NumberInput.Label>
-      <HStack gap="2">
-        <NumberInput.DecrementTrigger asChild>
-          <IconButton variant="outline" size="sm">
-            <LuMinus />
-          </IconButton>
-        </NumberInput.DecrementTrigger>
-        <NumberInput.ValueText textAlign="center" fontSize="lg" minW="3ch" />
-        <NumberInput.IncrementTrigger asChild>
-          <IconButton variant="outline" size="sm">
-            <LuPlus />
-          </IconButton>
-        </NumberInput.IncrementTrigger>
-      </HStack>
-    </NumberInput.Root>
+      <Flex justify="space-between" align="center" mb="2">
+        <Slider.Label>Set year data</Slider.Label>
+        <Slider.ValueText fontWeight="bold" />
+      </Flex>
+      <Slider.Control>
+        <Slider.Track />
+        <Slider.Thumb
+          index={0}
+          width={2}
+          height={6}
+          bg="blue.muted"
+          border="1.5px solid"
+          borderColor="blue.solid"
+          cursor="pointer"
+          _dragging={{ cursor: "grab" }}
+          _active={{ cursor: "grab" }}
+        >
+          <Slider.HiddenInput />
+          <Slider.DraggingIndicator
+            layerStyle="fill.subtle"
+            bottom="6"
+            rounded="sm"
+            px="1.5"
+          >
+            <Slider.ValueText />
+          </Slider.DraggingIndicator>
+        </Slider.Thumb>
+        <Slider.Marks marks={marks} />
+      </Slider.Control>
+    </Slider.Root>
   );
 };
